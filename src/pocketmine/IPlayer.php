a7c2288647a358724609bc67eb752cee352e008a
SOFe <sofe2038@gmail.com>
2017-01-07 23:41:54 +0800

    Created pocketmine\player namespace with class aliases

--- src/pocketmine/IPlayer.php
+++ src/pocketmine/IPlayer.php
@@ -19,60 +19,6 @@
  *
 */
 
-namespace pocketmine;
+use pocketmine\player\IPlayer;
 
-use pocketmine\permission\ServerOperator;
-
-interface IPlayer extends ServerOperator{
-
-	/**
-	 * @return bool
-	 */
-	public function isOnline();
-
-	/**
-	 * @return string
-	 */
-	public function getName();
-
-	/**
-	 * @return bool
-	 */
-	public function isBanned();
-
-	/**
-	 * @param bool $banned
-	 */
-	public function setBanned($banned);
-
-	/**
-	 * @return bool
-	 */
-	public function isWhitelisted();
-
-	/**
-	 * @param bool $value
-	 */
-	public function setWhitelisted($value);
-
-	/**
-	 * @return Player|null
-	 */
-	public function getPlayer();
-
-	/**
-	 * @return int|double
-	 */
-	public function getFirstPlayed();
-
-	/**
-	 * @return int|double
-	 */
-	public function getLastPlayed();
-
-	/**
-	 * @return mixed
-	 */
-	public function hasPlayedBefore();
-
-}
+class_alias(IPlayer::class, "pocketmine\\IPlayer");
